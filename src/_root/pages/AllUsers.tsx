e9013631b036682b487b73319431822de2feaea3
marcelo dominguez <marceelodominguez@gmail.com>
2024-07-23 10:34:39 -0300

    page all users

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,3 +1,35 @@
+import Loader from "@/components/shared/Loader";
+import UserCard from "@/components/shared/UserCard";
+import { useToast } from "@/components/ui/use-toast";
+import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
+
 export default function AllUsers() {
-  return <div>AllUsers</div>;
+  const { toast } = useToast();
+
+  const { data: users, isLoading, isError: isErrorUsers } = useGetUsers();
+
+  if (isErrorUsers) {
+    toast({ title: "Something went wrong." });
+
+    return;
+  }
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {isLoading && !users ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {users?.documents.map((user) => (
+              <li key={user?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={user} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
