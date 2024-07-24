059aaee3e17d8e9da109e06ebeb6a2199b67a430
marcelo dominguez <marceelodominguez@gmail.com>
2024-07-24 12:25:24 -0300

    page saved

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -64,5 +64,5 @@ export const multiFormatDateString = (timestamp: string = ""): string => {
 };
 
 export const checkIsLiked = (likeList: string[], userId: string) => {
-  return likeList.includes(userId);
+  return likeList?.includes(userId);
 };
